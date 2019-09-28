f7ebc48a32929a3a9ff0ea75fe9cfcf64b703c11
ZainNasrullah <zain.nasrullah.zn@gmail.com>
2019-09-28 19:09:40 -0400

    updated requirements to include sphinx and setup to include numba

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=[
     "matplotlib==3.1.1",
+    "numba==0.45.1",
     "numpy==1.17.2",
     "pandas==0.25.1",
     "scikit-learn==0.21.3",
